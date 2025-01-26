Fixed RMSE Calculation and added model saving

models/abc_bigru.py
@@ -30,7 +30,7 @@ def evaluate_hyperparameters(params, train_dataloader, val_dataloader, device):
         for batch_X, batch_y in train_dataloader:
             optimizer.zero_grad()
             outputs = model(batch_X.unsqueeze(1).to(device))
-            loss = criterion(outputs.squeeze(), batch_y)
+            loss = criterion(outputs.squeeze(), batch_y.to(device))
             loss.backward()
             optimizer.step()
 
@@ -40,7 +40,7 @@ def evaluate_hyperparameters(params, train_dataloader, val_dataloader, device):
     with torch.no_grad():
         for batch_X, batch_y in val_dataloader:
             outputs = model(batch_X.unsqueeze(1).to(device)).squeeze()
-            loss = criterion(outputs, batch_y)
+            loss = criterion(outputs, batch_y.to(device))
             total_loss += loss.item()
 
     return total_loss / len(val_dataloader)  # Return average MSE as fitness
@@ -51,10 +51,10 @@ def modify_solution(solution, bounds, factor=0.1):
     new_solution = solution + np.random.uniform(-factor, factor, size=len(solution))
     return np.clip(new_solution, bounds[:, 0], bounds[:, 1])
 
-def abc_algorithm(bounds, train_dataloader, val_dataloader, n_solutions=10, max_iters=20):
+def abc_algorithm(bounds, train_dataloader, val_dataloader, device, n_solutions=10, max_iters=20):
     n_params = bounds.shape[0]
     solutions = np.random.uniform(bounds[:, 0], bounds[:, 1], size=(n_solutions, n_params))
-    fitness = np.array([evaluate_hyperparameters(s, train_dataloader, val_dataloader) for s in solutions])
+    fitness = np.array([evaluate_hyperparameters(s, train_dataloader, val_dataloader, device) for s in solutions])
 
     best_solution = solutions[np.argmin(fitness)]
     best_fitness = np.min(fitness)
@@ -63,7 +63,7 @@ def abc_algorithm(bounds, train_dataloader, val_dataloader, n_solutions=10, max_
         for i in range(n_solutions):
             # Employed bee phase
             new_solution = modify_solution(solutions[i], bounds)
-            new_fitness = evaluate_hyperparameters(new_solution, train_dataloader, val_dataloader)
+            new_fitness = evaluate_hyperparameters(new_solution, train_dataloader, val_dataloader, device)
             if new_fitness < fitness[i]:
                 solutions[i] = new_solution
                 fitness[i] = new_fitness
@@ -73,7 +73,7 @@ def abc_algorithm(bounds, train_dataloader, val_dataloader, n_solutions=10, max_
         for i in range(n_solutions):
             if np.random.rand() < probabilities[i]:
                 new_solution = modify_solution(solutions[i], bounds)
-                new_fitness = evaluate_hyperparameters(new_solution, train_dataloader, val_dataloader)
+                new_fitness = evaluate_hyperparameters(new_solution, train_dataloader, val_dataloader, device)
                 if new_fitness < fitness[i]:
                     solutions[i] = new_solution
                     fitness[i] = new_fitness
@@ -82,7 +82,7 @@ def abc_algorithm(bounds, train_dataloader, val_dataloader, n_solutions=10, max_
         if np.random.rand() < 0.1:  # 10% chance to explore randomly
             random_index = np.random.randint(n_solutions)
             solutions[random_index] = np.random.uniform(bounds[:, 0], bounds[:, 1], size=n_params)
-            fitness[random_index] = evaluate_hyperparameters(solutions[random_index], train_dataloader, val_dataloader)
+            fitness[random_index] = evaluate_hyperparameters(solutions[random_index], train_dataloader, val_dataloader, device)
 
         # Update the best solution
         current_best_index = np.argmin(fitness)

models/cnn_dblstm.py
@@ -9,15 +9,15 @@ class CNN_Block(torch.nn.Module):
 
         # Block 1
         self.cnn_1 = torch.nn.Conv1d(in_channels=1, out_channels=hidden_channels[0], padding='same', kernel_size=5)
-        self.bn_1 = torch.nn.BatchNorm1d(num_features=hidden_channels)
+        self.bn_1 = torch.nn.BatchNorm1d(num_features=hidden_channels[0])
 
         # Block 2
         self.cnn_2 = torch.nn.Conv1d(in_channels=hidden_channels[0], out_channels=hidden_channels[1], padding='same', kernel_size=3)
-        self.bn_2 = torch.nn.BatchNorm1d(num_features=hidden_channels)
+        self.bn_2 = torch.nn.BatchNorm1d(num_features=hidden_channels[1])
 
         # Block 3
         self.cnn_3 = torch.nn.Conv1d(in_channels=hidden_channels[1], out_channels=hidden_channels[2], padding='same', kernel_size=2)
-        self.bn_3 = torch.nn.BatchNorm1d(num_features=hidden_channels)
+        self.bn_3 = torch.nn.BatchNorm1d(num_features=hidden_channels[2])
 
     def forward(self, x):
         # Block 1
@@ -41,7 +41,7 @@ class CNN_DBLSTM_Model(torch.nn.Module):
     def __init__(self, cnn_hidden_dims, lstm_hidden_dims, lstm_layers):
         super().__init__()
         self.cnn_block = CNN_Block(cnn_hidden_dims)
-        self.lstm_block = torch.nn.LSTM(cnn_hidden_dims, hidden_size=lstm_hidden_dims, num_layers=lstm_layers, batch_first=True, bidirectional=True)
+        self.lstm_block = torch.nn.LSTM(cnn_hidden_dims[-1], hidden_size=lstm_hidden_dims, num_layers=lstm_layers, batch_first=True, bidirectional=True)
         self.fc = torch.nn.Sequential(
             torch.nn.Linear(2*lstm_hidden_dims, 64),
             torch.nn.ReLU(),